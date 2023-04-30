69ef85dec249c7306dbba50da0a4110c88d95c42
Sergei-Gerasimenko <110779620+Sergei-Gerasimenko@users.noreply.github.com>
2023-04-30 22:27:09 +0300

    feat:
    downloaded webpack

---
+++ source/js/components/viewport-height.js
@@ -0,0 +1,26 @@
+// https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+
+class ViewportHeight {
+    constructor() {
+        this.onResize = this.onResize.bind(this);
+    }
+
+    init() {
+        window.addEventListener('resize', this.onResize);
+    }
+
+    updateHeight() {
+        const viewportHeight = window.visualViewport ? window.visualViewport.height : window.innerHeight;
+        const vh = viewportHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
+    onResize() {
+        this.updateHeight();
+    }
+}
+
+const instance = new ViewportHeight();
+instance.init();
+
+export default instance;
